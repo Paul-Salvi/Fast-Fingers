3a6c477207761a597d8d7bcfd033ca1b03be6539
Paul <plslv1991@gmail.com>
2021-02-21 11:05:44 +0530

    Deployment package

---
+++ precache-manifest.99188ecc895fa995f33496ac7de180e9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e67c68c17737d5c343c7acc959eb211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca888dfc70b8eae0dffc",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "4c54c4c181df2c0a91b5",
+    "url": "/static/css/main.40da276b.chunk.css"
+  },
+  {
+    "revision": "ca888dfc70b8eae0dffc",
+    "url": "/static/js/2.6a701b10.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6a701b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c54c4c181df2c0a91b5",
+    "url": "/static/js/main.dfd16601.chunk.js"
+  },
+  {
+    "revision": "3cc9b9fc6dc1b4bb924e",
+    "url": "/static/js/runtime-main.519ac2c7.js"
+  },
+  {
+    "revision": "e32312a7bf00e36cc88b3787b8b36b48",
+    "url": "/static/media/background.e32312a7.png"
+  },
+  {
+    "revision": "465bb1673f9f4de64d7cc1d7fa086985",
+    "url": "/static/media/cross-icon.465bb167.svg"
+  },
+  {
+    "revision": "0cd364ed5f73e9674fabbc73453de723",
+    "url": "/static/media/gamepad-icon.0cd364ed.svg"
+  },
+  {
+    "revision": "9c1f982035aaa27323828a36b6811466",
+    "url": "/static/media/keyboard-icon.9c1f9820.svg"
+  },
+  {
+    "revision": "c589b8d1b4823b668a4780e8b65a1bf2",
+    "url": "/static/media/person-icon.c589b8d1.svg"
+  },
+  {
+    "revision": "c6ad28220df4300d88832ae3f6835a7c",
+    "url": "/static/media/play-icon.c6ad2822.svg"
+  },
+  {
+    "revision": "7c71c1abb2fa43afa27c621e1f91e9bb",
+    "url": "/static/media/reload-icon.7c71c1ab.svg"
+  }
+]);
